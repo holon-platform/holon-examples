0bc08be2033de9b19c27a8abfa616c2313cf87b2
Riccardo Righi <rrighi@holon-platform.com>
2017-07-10 19:31:02 +0200

    auth basic example

---
+++ jax-rs/spring-boot-auth-basic/src/main/java/com/holonplatform/example/jaxrs/springboot/auth/basic/Application.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2000-2017 Holon TDCN.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.holonplatform.example.jaxrs.springboot.auth.basic;
+
+import java.util.Optional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.holonplatform.auth.Account;
+import com.holonplatform.auth.Account.AccountProvider;
+import com.holonplatform.auth.AuthenticationToken;
+import com.holonplatform.auth.Credentials;
+import com.holonplatform.auth.Realm;
+
+@SpringBootApplication
+public class Application {
+
+	@Bean
+	public Realm realm() {
+		return Realm.builder()
+				// Basic HTTP authorization schema resolver
+				.resolver(AuthenticationToken.httpBasicResolver())
+				// authenticator using the accountProvider() service
+				.authenticator(Account.authenticator(accountProvider()))
+				// default authorizer
+				.withDefaultAuthorizer().build();
+	}
+
+	@Bean
+	public AccountProvider accountProvider() {
+		return accountId -> {
+			// act1
+			if ("act1".equals(accountId)) {
+				return Optional.of(Account.builder(accountId)
+						// set credentials
+						.credentials(Credentials.builder().secret("act1pwd").build())
+						// configuration
+						.enabled(true)
+						// add details
+						.detail("name", "Account 1")
+						// permission roles
+						.permissionStrings("ROLE1").build());
+			}
+			// act2
+			if ("act2".equals(accountId)) {
+				return Optional.of(Account.builder(accountId)
+						// set credentials
+						.credentials(Credentials.builder().secret("act2pwd").build())
+						// configuration
+						.enabled(true)
+						// add details
+						.detail("name", "Account 2")
+						// permission roles
+						.permissionStrings("ROLE2").build());
+			}
+			return Optional.empty();
+		};
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ jax-rs/spring-boot-auth-basic/src/test/java/com/holonplatform/example/jaxrs/springboot/auth/basic/test/Client.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2000-2017 Holon TDCN.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.holonplatform.example.jaxrs.springboot.auth.basic.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.holonplatform.auth.Realm;
+import com.holonplatform.http.HttpStatus;
+import com.holonplatform.http.rest.ResponseEntity;
+import com.holonplatform.http.rest.RestClient;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class Client {
+
+	@LocalServerPort
+	private int serverPort;
+
+	@Autowired
+	private Realm realm;
+
+	@Test
+	public void testClient() {
+
+		Assert.assertNotNull(realm);
+
+		RestClient client = RestClient.forTarget("http://localhost:" + serverPort + "/api/");
+
+		// this should not be authorized
+		ResponseEntity<String> response = client.request().path("anyrole").get(String.class);
+		Assert.assertEquals(HttpStatus.UNAUTHORIZED, response.getStatus());
+		response.close();
+
+		// authenticate as act1
+		response = client.request().path("anyrole").authorizationBasic("act1", "act1pwd").get(String.class);
+		// now should be OK
+		Assert.assertEquals(HttpStatus.OK, response.getStatus());
+		response.close();
+
+		// act1 has the ROLE1
+		response = client.request().path("role1").authorizationBasic("act1", "act1pwd").get(String.class);
+		Assert.assertEquals(HttpStatus.OK, response.getStatus());
+		response.close();
+
+		// act1 has NOT the ROLE2
+		response = client.request().path("role2").authorizationBasic("act1", "act1pwd").get(String.class);
+		Assert.assertEquals(HttpStatus.FORBIDDEN, response.getStatus());
+		response.close();
+
+		// authenticate as act2, which has the ROLE2
+		response = client.request().path("role2").authorizationBasic("act2", "act2pwd").get(String.class);
+		Assert.assertEquals(HttpStatus.OK, response.getStatus());
+		response.close();
+
+	}
+
+}
