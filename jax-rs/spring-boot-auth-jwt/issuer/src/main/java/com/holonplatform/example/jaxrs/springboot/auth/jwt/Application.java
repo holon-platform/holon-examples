ed8c53de3eefaa647ee9f514e10cb3f2ba8ec1ef
Riccardo Righi <rrighi@holon-platform.com>
2017-07-11 17:46:08 +0200

    Use JWT auto configuration

--- jax-rs/spring-boot-auth-jwt/issuer/src/main/java/com/holonplatform/example/jaxrs/springboot/auth/jwt/Application.java
+++ jax-rs/spring-boot-auth-jwt/issuer/src/main/java/com/holonplatform/example/jaxrs/springboot/auth/jwt/Application.java
@@ -15,25 +15,17 @@
  */
 package com.holonplatform.example.jaxrs.springboot.auth.jwt;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 import com.holonplatform.auth.Account;
 import com.holonplatform.auth.AuthenticationToken;
 import com.holonplatform.auth.Realm;
-import com.holonplatform.auth.jwt.JwtConfigProperties;
-import com.holonplatform.auth.jwt.JwtConfiguration;
-import com.holonplatform.spring.EnvironmentConfigPropertyProvider;
 
 @SpringBootApplication
 public class Application {
 
-	@Autowired
-	private Environment environment;
-	
 	@Bean
 	public Realm realm(AccountService accountProvider) {
 		return Realm.builder()
@@ -43,13 +35,6 @@ public class Application {
 				.authenticator(Account.authenticator(accountProvider))
 				// default authorizer
 				.withDefaultAuthorizer().build();
-	}
-	
-	// JWT configuration from properties
-	@Bean
-	public JwtConfiguration jwtConfiguration() {
-		return JwtConfiguration.build(JwtConfigProperties.builder()
-				.withPropertySource(EnvironmentConfigPropertyProvider.create(environment)).build());
 	}
 
 	public static void main(String[] args) {
--- jax-rs/spring-boot-auth-jwt/server/src/main/java/com/holonplatform/example/jaxrs/springboot/auth/jwt/Application.java
+++ jax-rs/spring-boot-auth-jwt/server/src/main/java/com/holonplatform/example/jaxrs/springboot/auth/jwt/Application.java
@@ -15,41 +15,26 @@
  */
 package com.holonplatform.example.jaxrs.springboot.auth.jwt;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 import com.holonplatform.auth.AuthenticationToken;
 import com.holonplatform.auth.Realm;
 import com.holonplatform.auth.jwt.JwtAuthenticator;
-import com.holonplatform.auth.jwt.JwtConfigProperties;
 import com.holonplatform.auth.jwt.JwtConfiguration;
-import com.holonplatform.spring.EnvironmentConfigPropertyProvider;
 
 @SpringBootApplication
 public class Application {
 
-	@Autowired
-	private Environment environment;
-
 	@Bean
-	public Realm realm() {
+	public Realm realm(JwtConfiguration jwtConfiguration) {
 		return Realm.builder()
 				// HTTP Bearer authorization schema resolver
 				.resolver(AuthenticationToken.httpBearerResolver())
 				// authenticator using JwtConfiguration and allowing only 'TestIssuer' issuer name
-				.authenticator(
-						JwtAuthenticator.builder().configuration(jwtConfiguration()).issuer("TestIssuer").build())
+				.authenticator(JwtAuthenticator.builder().configuration(jwtConfiguration).issuer("TestIssuer").build())
 				.build();
-	}
-
-	// JWT configuration from properties
-	@Bean
-	public JwtConfiguration jwtConfiguration() {
-		return JwtConfiguration.build(JwtConfigProperties.builder()
-				.withPropertySource(EnvironmentConfigPropertyProvider.create(environment)).build());
 	}
 
 	public static void main(String[] args) {
