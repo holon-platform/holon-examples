82f0054ad44b22726ad2207863017881e41c45be
Riccardo Righi <rr9379@gmail.com>
2017-12-18 10:25:37 +0100

    Update platform version to 5.0.6

--- ui-vaadin/simple-app/src/main/java/com/holonplatform/example/ui/vaadin/app/views/Home.java
+++ ui-vaadin/simple-app/src/main/java/com/holonplatform/example/ui/vaadin/app/views/Home.java
@@ -25,7 +25,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.holonplatform.core.datastore.Datastore;
 import com.holonplatform.vaadin.components.Components;
+import com.holonplatform.vaadin.components.PropertyListing;
 import com.holonplatform.vaadin.navigator.ViewNavigator;
+import com.holonplatform.vaadin.navigator.annotations.OnShow;
 import com.holonplatform.vaadin.spring.DefaultView;
 import com.vaadin.navigator.View;
 import com.vaadin.spring.annotation.SpringView;
@@ -43,6 +45,8 @@ public class Home extends VerticalLayout implements View {
 	@Autowired
 	private Datastore datastore;
 
+	private PropertyListing listing;
+
 	@PostConstruct
 	public void init() {
 		Components.configure(this)
@@ -52,7 +56,7 @@ public class Home extends VerticalLayout implements View {
 						// navigate to "manage" view
 						.onClick(e -> ViewNavigator.require().toView("manage").navigate()).build())
 				// build and add listing
-				.addAndExpandFull(Components.listing.properties(PRODUCT)
+				.addAndExpandFull(listing = Components.listing.properties(PRODUCT)
 						// setup data source using Datastore with 'products' table name target and product ID as pk
 						.dataSource(datastore, TARGET, ID)
 						// froze the ID column
@@ -64,6 +68,12 @@ public class Home extends VerticalLayout implements View {
 								.withParameter("id", i.getValue(ID)).navigate())
 						// set full size and build
 						.fullSize().build());
+	}
+
+	@OnShow
+	public void onShow() {
+		// refresh listing at view display
+		listing.refresh();
 	}
 
 }
